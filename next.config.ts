d18522d5b8ee35bfff315d6dae875e8d374151ff
Ali Hussein <ali.hussein@live.com>
2025-09-20 23:30:31 +0300

    Updated the project for deployment on GitHub

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  basePath: '/qr-code-component',
+  assetPrefix: '/qr-code-component/',
+  images: {
+    unoptimized: true
+  }
+}
 
 export default nextConfig;
